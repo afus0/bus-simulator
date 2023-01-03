82fb981cc0ad023a9a6ef62a048a8dea9ba44946
Andrea Fuso <andrea.fuso2@gmail.com>
2023-01-03 11:10:16 +0100

    aggiornato il file gmaps.js

--- WebContent/js/gmaps.js
+++ WebContent/js/gmaps.js
@@ -1,14 +1,14 @@
-// Initialize and add the map
+// Funzione che fa partire maps
 function initMap() {
-  // The location of Uluru
-  var position = [45.46487396906016, 9.193401838780664];
+  // Posizione di partenza 
+  var position = [45.466396217241524, 9.197577245397706];
   const partenza = { lat: position[0], lng: position[1] };
-  // The map, centered at Uluru
+  // La mappa che centra la posizione di partenza
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
     center: partenza,
   });
-  // The marker, positioned at Uluru
+  // Creazione del marker "partenza"
   var marker_partenza = new google.maps.Marker({
     position: partenza,
     map: map,
@@ -16,17 +16,15 @@ function initMap() {
       url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
     }
   });
-  /*const marker_b = new google.maps.Marker({
-    position: { lat: 45.48517073702312, lng: 9.193401838780664 },
-    map: map
-  })*/
+
+  // Creazione array di markers
   var fermate = [
     new google.maps.Marker({
-      position: { lat: 45.46487396906016, lng: 9.193401838780664 },
+      position: { lat: 45.466396217241524, lng: 9.197577245397706 },
       map: map
     }),
     new google.maps.Marker({
-      position: { lat: 45.466396217241524, lng: 9.197577245397706 },
+      position: { lat: 45.46487396906016, lng: 9.193401838780664 },
       map: map
     }),
     new google.maps.Marker({
@@ -50,8 +48,9 @@ function initMap() {
       map: map
     }),
   ]
+
+  //Creazione di un semplice array con le cordinate dei markers di riga  21;
   const fermate2 = [
-    { lat: 45.46487396906016, lng: 9.193401838780664 },
     { lat: 45.466396217241524, lng: 9.197577245397706 },
     { lat: 45.46487396906016, lng: 9.193401838780664 },
     { lat: 45.4647340644238, lng: 9.192724657101353 },
@@ -60,6 +59,8 @@ function initMap() {
     { lat: 45.46793829799989, lng: 9.176783765776795 },
     { lat: 45.46191346841282, lng: 9.173412006263305 }
   ]
+
+  // Creazione linea rossa
   const bus1 = new google.maps.Polyline({
     path: fermate2,
     geodesic: true,
@@ -67,44 +68,65 @@ function initMap() {
     strokeOpacity: 1.0,
     strokeWeight: 2,
   });
+
+  // linea rossa viene impostato sulla mappa
   bus1.setMap(map);
-  google.maps.event.addListener(map, 'DOMContentLoaded', function (event) {
-    var result = [event.latLng.lat(), event.latLng.lng()];
-    transition(result);
-  });
 
-  function changeMarkerPosition(marker) {
-    var latlng = new google.maps.LatLng(45.46692879073799, 9.195413305459017);
+
+
+  /*function changeMarkerPosition(marker) {
+    var latlng = new google.maps.LatLng(45.46191346841282, 9.173412006263305);
     marker.setPosition(latlng);
-}
-changeMarkerPosition(marker_partenza);
-
- /* var numDeltas = 100;
-  var delay = 10; //milliseconds
-  var i = 0;
-  var deltaLat;
-  var deltaLng;
-
- 
-
-
-  function transition(result) {
-    i = 0;
-    deltaLat = (result[0] - position[0]) / numDeltas;
-    deltaLng = (result[1] - position[1]) / numDeltas;
-    moveMarker();
-  }
-
-  function moveMarker() {
-    position[0] += deltaLat;
-    position[1] += deltaLng;
-    var partenza = new google.maps.LatLng(position[0], position[1]);
-    marker.setTitle("Latitude:" + position[0] + " | Longitude:" + position[1]);
-    marker.setPosition(partenza);
-    if (i != numDeltas) {
-      i++;
-      setTimeout(moveMarker, delay);
-    }
   }*/
+
+
+  // creo una funzione con i che parte da 0 e continua con l'intervallo
+
+  var i = 0;
+  // funzione che muove il bus settando una nuova posizione nel marker_partenza
+  function moveBus() {
+    marker_partenza.setPosition(new google.maps.LatLng(fermate2[i].lat, fermate2[i].lng));
+    i++;
+  }
+  setInterval(moveBus, 1000);
 }
 window.initMap = initMap;
+
+
+
+
+
+
+
+  //moveBus(marker_partenza);
+
+
+  //changeMarkerPosition(marker_partenza);
+
+  /* var numDeltas = 100;
+   var delay = 10; //milliseconds
+   var i = 0;
+   var deltaLat;
+   var deltaLng;
+ 
+  
+ 
+ 
+   function transition(result) {
+     i = 0;
+     deltaLat = (result[0] - position[0]) / numDeltas;
+     deltaLng = (result[1] - position[1]) / numDeltas;
+     moveMarker();
+   }
+ 
+   function moveMarker() {
+     position[0] += deltaLat;
+     position[1] += deltaLng;
+     var partenza = new google.maps.LatLng(position[0], position[1]);
+     marker.setTitle("Latitude:" + position[0] + " | Longitude:" + position[1]);
+     marker.setPosition(partenza);
+     if (i != numDeltas) {
+       i++;
+       setTimeout(moveMarker, delay);
+     }
+   }*/
