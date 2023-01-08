8928eff909771eb19f911891d35626c5e4a01e87
Andrea Fuso <andrea.fuso2@gmail.com>
2023-01-08 11:23:00 +0100

    test

--- WebContent/js/gmaps.js
+++ WebContent/js/gmaps.js
@@ -1146,6 +1146,7 @@ function initMap() {
   var bus_interval_green = setInterval(moveBusGreen, 1000);
   var bus_interval_blu = setInterval(moveBusBlu, 1000);
 
+// Add an event listener to the marker
 
   ////////////////////////////////CHECK BOX
   this.checkbox = [
@@ -1305,6 +1306,50 @@ function initMap() {
   // Bind the Autocomplete object to the search input
   var autocomplete = new google.maps.places.Autocomplete(input);
 
+  
+
+
+
+
+
+
+
+
+
+
+  /// CREAZIONE INFOWINDOW (popup delle fermate)
+  const contentString =
+  '<div id="content">' +
+  '<div id="siteNotice">' +
+  "</div>" +
+  '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+  '<div id="bodyContent">' +
+  "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+  "sandstone rock formation in the southern part of the " +
+  "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+  "south west of the nearest large town, Alice Springs; 450&#160;km " +
+  "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+  "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+  "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+  "Aboriginal people of the area. It has many springs, waterholes, " +
+  "rock caves and ancient paintings. Uluru is listed as a World " +
+  "Heritage Site.</p>" +
+  '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+  "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+  "(last visited June 22, 2009).</p>" +
+  "</div>" +
+  "</div>";
+// Create the info window
+var infoWindow = new google.maps.InfoWindow();
+
+// Set the content of the info window
+infoWindow.setContent('<h1>My Info Window</h1><p>Some content</p>');
+
+// Add an event listener to the marker
+google.maps.event.addListener(markerFermateRossa[0], 'click', function() {
+// Open the info window
+infoWindow.open(map, markerFermateRossa[0]);
+});
 }
 // fa partire la mappa di google all'avvio della pagina
 window.initMap = initMap;
--- src/controller/home.java
+++ src/controller/home.java
@@ -28,7 +28,8 @@ public class home extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String path = request.getPathInfo();
+		response.sendRedirect("index.html");
 	}
 
 	/**
@@ -36,7 +37,6 @@ public class home extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
